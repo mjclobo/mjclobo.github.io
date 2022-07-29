f5aba0d6e419b3526b4a4bbdf44b7cc474736035
Matt Lobo <ml1994@princeton.edu>
2022-07-29 17:49:04 -0400

    trying to fix broken page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Introduction",
         "excerpt":"Hello! I’m a PhD student with a strong interest in studying how to better describe and model the earth system. My focus is on ocean physics and ocean/climate modeling, though I have a strong drive to develop and expand my knowledge in all adjacent subjects such as atmospheric physics, air-sea...","categories": ["Intro"],
         "tags": ["hello","oceanography","physics","fourth"],
-        "url": "/mjclobo.github.io/intro/profile-overview/",
+        "url": "/intro/profile-overview/",
         "teaser": null
       }]
